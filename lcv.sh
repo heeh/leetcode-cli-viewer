aeea819ad4f491f05b0bbcb51db4ad276b3b422a
heeh <heehcs@gmail.com>
2021-05-15 12:47:14 -0400

    Output check with 0

--- lcv.sh
+++ lcv.sh
@@ -23,13 +23,18 @@ PROB_OPT=(1 "Show Problem"
 
 function promptList() {
     LIST_CHOICE=$(dialog --clear \
-                    --backtitle "$BACKTITLE" \
-                    --title "$TITLE" \
-                    --menu "$MENU" \
-                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                    "${LIST_OPT[@]}" \
-                    2>&1 >/dev/tty)
-    echo $LIST_CHOICE
+			 --backtitle "$BACKTITLE" \
+			 --title "$TITLE" \
+			 --menu "$MENU" \
+			 $HEIGHT $WIDTH $CHOICE_HEIGHT \
+			 "${LIST_OPT[@]}" \
+			 2>&1 >/dev/tty)
+    if [ $? -eq 0 ]
+    then
+	echo $LIST_CHOICE
+    else
+	break
+    fi
 }
 
 function loadList() {
@@ -53,45 +58,57 @@ function processList() {
     PROB_NUMBER=$(dialog --menu "$MENU" $HEIGHT $WIDTH $CHOICE_HEIGHT \
 			 "${PROBLEMS[@]}" \
 			 2>&1 >/dev/tty)
-    echo $PROB_NUMBER
+    if [ $? -eq 0 ]
+    then
+	echo $PROB_NUMBER
+    else
+	break
+    fi
 }
 
 function processProblem() {
     PROB_NUMBER=$1
     PROB_ACTION=$(dialog --menu "Problem Number: $PROB_NUMBER" $HEIGHT $WIDTH $CHOICE_HEIGHT \
 			 "${PROB_OPT[@]}" \
-			 2>&1 >/dev/tty)     
-    case $PROB_ACTION in
-	1)
-#	    leetcode pick "${PROB_NUMBER}" > prob.txt
-	    leetcode pick "${PROB_NUMBER}" | fold -w 80 -s > prob.txt
-	    dialog --textbox prob.txt $HEIGHT $WIDTH
-	    ;;
-	2)
-	    leetcode edit "${PROB_NUMBER}" > prob.txt
-	    ;;
-	3)
-	    leetcode test "${PROB_NUMBER}" > prob.txt
-	    dialog --textbox prob.txt $HEIGHT $WIDTH
-	    ;;
-	4)
-	    leetcode exec "${PROB_NUMBER}" > prob.txt
-	    dialog --textbox prob.txt $HEIGHT $WIDTH
-	    ;;
-	5)
-	    PROB_NUMBER=$(processList)
-	    ;;
-	6)
-	    LIST_CHOICE=$(promptList)
-	    clear
-	    loadList $LIST_CHOICE	    
-	    ;;
-	7)
-	    clear
-	    exit
-	    ;;
-    esac    
+			 2>&1 >/dev/tty)
+    if [ $? -eq 0 ]
+    then
+	case $PROB_ACTION in
+	    1)
+		#	    leetcode pick "${PROB_NUMBER}" > prob.txt
+		leetcode pick "${PROB_NUMBER}" | fold -w 80 -s > prob.txt
+		dialog --keep-tite --textbox prob.txt $HEIGHT $WIDTH
+		;;
+	    2)
+		leetcode edit "${PROB_NUMBER}" > prob.txt
+		;;
+	    3)
+		leetcode test "${PROB_NUMBER}" > prob.txt
+		dialog --textbox prob.txt $HEIGHT $WIDTH
+		;;
+	    4)
+		leetcode exec "${PROB_NUMBER}" > prob.txt
+		dialog --textbox prob.txt $HEIGHT $WIDTH
+		;;
+	    5)
+		PROB_NUMBER=$(processList)
+		;;
+	    6)
+		LIST_CHOICE=$(promptList)
+		clear
+		loadList $LIST_CHOICE	    
+		;;
+	    7)
+		clear
+		exit
+		;;
+	esac
+    else
+	break
+    fi
 }
+
+
 
 while true; do
     # If we have a problem number, process right away
@@ -113,5 +130,5 @@ while true; do
 	fi
     fi
 done
- 
+
 
